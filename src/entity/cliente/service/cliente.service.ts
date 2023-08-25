d34ffc5b6604e53cbc7ba07a0b42c95dc38177fc
marcelodias90 <diasteixeira1990@gmail.com>
2023-08-25 16:37:58 -0300

    <back>[refactor]: alteração de erros dinamicos

--- src/entity/cliente/service/cliente.service.ts
+++ src/entity/cliente/service/cliente.service.ts
@@ -1,39 +1,40 @@
-import { Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cliente } from 'src/entity/cliente/entities/cliente';
 import { ClienteDto } from 'src/entity/cliente/dto/cliente.dto';
 import { ClienteRepository } from 'src/entity/cliente/repository/cliente.Repository';
+import { IdNaoEncotradoExcecao } from 'src/presentation/errors/IdNaoEncotrado.excecao';
+import { ExistenteExcecao } from 'src/presentation/errors/Existente.excecao';
 
 @Injectable()
 export class ClienteService {
   constructor(
-    private clienteRepository: ClienteRepository
-  ) {}
+    private clienteRepository: ClienteRepository) { }
 
   async Listar(): Promise<Cliente[]> {
-     return await this.clienteRepository.Lista();
+    return await this.clienteRepository.Lista();
   }
 
   async Criar(cliente: ClienteDto): Promise<Cliente> {
     const existeCliente = await this.clienteRepository.buscaPorEmail(cliente.email)
-    if(existeCliente){
-      throw new Error('Já existe um Cliente com esse email!')
+    if (existeCliente) {
+      throw new ExistenteExcecao('Cliente', 'e-mail')
     }
-     return await this.clienteRepository.create(cliente)
- }
+    return await this.clienteRepository.create(cliente)
+  }
 
   async atualiza(id: number, cliente: ClienteDto): Promise<void> {
     const existeCliente = await this.clienteRepository.buscaPorId(id)
-    if(!existeCliente){
-      throw new Error('Não existe um Cliente com esse id!')
+    if (!existeCliente) {
+      throw new IdNaoEncotradoExcecao(id)
     }
-      await this.clienteRepository.update(id , cliente)
- }
+    await this.clienteRepository.update(id, cliente)
+  }
 
   async exclui(id: number): Promise<Cliente> {
     const existeCliente = await this.clienteRepository.buscaPorId(id)
-    if(!existeCliente){
-      throw new Error('Não existe um Cliente com esse id!')
+    if (!existeCliente) {
+      throw new IdNaoEncotradoExcecao(id)
     }
-     return await this.clienteRepository.deletar(existeCliente)
- }
+    return await this.clienteRepository.deletar(existeCliente)
+  }
 }
