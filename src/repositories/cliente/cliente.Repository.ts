2505c127e2599c50cba8608e8d611dd65b174efe
Marcelo <marcelo.squadra@squara.com.br>
2023-08-25 12:31:17 -0300

    <back>[feat]: criado  cliente repositoryr

---
+++ src/repositories/cliente/cliente.Repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'; 
+import { Cliente } from 'src/entity/cliente/cliente';
+import { ClienteDto } from 'src/entity/cliente/dto/cliente.dto';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ClienteRepository {
+  constructor(
+    @InjectRepository(Cliente)
+    private Repository: Repository<Cliente>,
+  ) {}
+
+  async Lista(): Promise<Cliente[]> {
+    return await this.Repository.find();
+ }
+
+  async buscaPorNome(nome: string): Promise<Cliente> {
+    return await this.Repository.findOne({
+        where: {
+            nome
+        }
+    });
+ }
+  async buscaPorId(id: number): Promise<Cliente> {
+    return await this.Repository.findOne({
+        where: {
+            id
+        }
+    });
+ }
+
+  async buscaPorEmail(email: string): Promise<Cliente> {
+    return await this.Repository.findOne({
+        where: {
+            email
+        }
+    });
+ }
+
+ async save(cliente: ClienteDto): Promise<Cliente> {
+  const novoCliente = await this.Repository.save(cliente);
+   return  novoCliente;
+ }
+}
