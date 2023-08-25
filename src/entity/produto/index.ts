f92131921858ed7c35627287d42da73a10b25f2f
marcelodias90 <diasteixeira1990@gmail.com>
2023-08-25 18:10:51 -0300

    <back>[feat]: movido exportaçãos produto  para index.ts

---
+++ src/entity/produto/index.ts
@@ -0,0 +1,5 @@
+export * from "./controllers/produto.controller"
+export * from "./dto/produto.dto"
+export * from "./entities/produto"
+export * from "./repository/produto.Repository"
+export * from "./service/produto.service"
