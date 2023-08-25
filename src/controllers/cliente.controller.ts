a5ed1c529812742e4ab66dd14a00a0a934fd2d19
Marcelo <marcelo.squadra@squara.com.br>
2023-08-25 12:30:53 -0300

    <back>[feat]: criado  cliente controller

---
+++ src/controllers/cliente.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body} from '@nestjs/common';
+import { Cliente } from 'src/entity/cliente/cliente';
+import { ClienteDto } from 'src/entity/cliente/dto/cliente.dto';
+import { ClienteService } from 'src/service/cliente/cliente.service';
+
+@Controller('cliente')
+export class clienteController {
+    constructor(private readonly clienteService: ClienteService) { }
+
+    @Get('lista')
+    async Listar(): Promise<Cliente[]> {
+        return await this.clienteService.Listar();
+    }
+
+    @Post('criar')
+    async Criar(@Body() cliente: ClienteDto) : Promise<Cliente>{
+       return await  this.clienteService.Criar(cliente)
+    }
+}
