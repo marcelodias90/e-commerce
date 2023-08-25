c0f84a47abf1a0d43512d15d9f6eed2fadbbc5ad
Marcelo <marcelo.squadra@squara.com.br>
2023-08-25 11:59:19 -0300

    <back>[feat]: migrações produto, cliente

---
+++ src/entity/produto/produto.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('produto')
+export class Cliente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  nome: string;
+
+  @Column('int')
+  preco: Number;
+
+  @Column({ length: 100 })
+  email: string;
+}
---
+++ src/migration/1692922485697-Create_Cliente_Table.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateClienteTable1692922485697 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "cliente",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "nome",
+                        type: "varchar",
+                    },
+                    {
+                        name: "cpf",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isUnique: true
+                    },
+                ],
+            }),
+            true,
+        )
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('cliente')
+    }
+
+}
