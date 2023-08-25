cef9389d5c4735fad4b71b4ba78efdc8d691b83d
Marcelo <marcelo.squadra@squara.com.br>
2023-08-25 12:29:19 -0300

    <back>[feat]: module e main

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ClienteModule } from './modules/cliente.modules';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env']
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 1433,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [`${__dirname}/entity/**/{.ts,*.js}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
+      extra: {
+        trustServerCertificate: true,
+      }
+
+    }),
+    ClienteModule,
+  ],
+
+})
+export class AppModule { }
